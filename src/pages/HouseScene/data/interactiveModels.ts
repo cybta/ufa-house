e4dc0aaaf3bdc200d17ee4ac23a949b48d7de256
cybta <c.saad@live.com>
2026-01-13 16:12:23 +0500

    organaize and start adding sockets

---
+++ src/pages/HouseScene/data/interactiveModels.ts
@@ -0,0 +1,10 @@
+import type { ThreeModel } from '../../../types';
+
+export const interactiveModels: ThreeModel[] = [
+  {
+    name: 'entrance-light-switch',
+    model: '/3dmodels/sockets/single-switch.glb',
+    position: [-4.15, 1.6, 4.25],
+    rotation: [Math.PI / 2, 0, Math.PI / 2],
+  },
+];
