b01761eb177a71ccf627024a7e3552d3fb7d5d5d
cybta <c.saad@live.com>
2026-01-17 16:33:11 +0500

    add all switches fix the entrance and the house objects

--- src/pages/HouseScene/data/interactiveModels.ts
+++ src/pages/HouseScene/data/interactiveModels.ts
@@ -4,7 +4,91 @@ export const interactiveModels: ThreeModel[] = [
   {
     name: 'entrance-light-switch',
     model: '/3dmodels/sockets/single-switch.glb',
-    position: [-4.15, 1.6, 4.25],
+    position: [-4.15, 1.6, 5.4],
+    rotation: [Math.PI / 2, 0, Math.PI / 2],
+  },
+
+  {
+    name: 'UtilityRoom-light-switch',
+    model: '/3dmodels/sockets/single-switch.glb',
+    position: [-5.9, 1.6, 6.02],
+    rotation: [Math.PI / 2, 0, 0],
+  },
+
+  {
+    name: 'Hall-light-switch',
+    model: '/3dmodels/sockets/single-switch.glb',
+    position: [-6.5, 1.6, 4.1],
+    rotation: [Math.PI / 2, 0, 0],
+  },
+
+  {
+    name: 'StairsDown-light-switch',
+    model: '/3dmodels/sockets/single-switch.glb',
+    position: [-8, 1.6, 4.1],
+    rotation: [Math.PI / 2, 0, 0],
+  },
+
+  {
+    name: 'StairsUp-light-switch',
+    model: '/3dmodels/sockets/single-switch.glb',
+    position: [-8, 4.6, 5.9],
+    rotation: [Math.PI / 2, 0, -Math.PI / 1],
+  },
+
+  {
+    name: 'Kitchen-light-switch',
+    model: '/3dmodels/sockets/single-switch.glb',
+    position: [-6.5, 1.6, 3.75],
+    rotation: [Math.PI / 2, 0, -Math.PI / 1],
+  },
+
+  {
+    name: 'LivingRoom-light-switch',
+    model: '/3dmodels/sockets/single-switch.glb',
+    position: [-8, 1.6, 3.75],
+    rotation: [Math.PI / 2, 0, -Math.PI / 1],
+  },
+
+  {
+    name: 'GuestRoom-light-switch',
+    model: '/3dmodels/sockets/single-switch.glb',
+    position: [-8.3, 1.6, 6.15],
+    rotation: [Math.PI / 2, 0, 0],
+  },
+
+  {
+    name: 'BedRoomA-light-switch',
+    model: '/3dmodels/sockets/single-switch.glb',
+    position: [-8, 4.6, 6.05],
+    rotation: [Math.PI / 2, 0, 0],
+  },
+
+  {
+    name: 'BigRoom-light-switch',
+    model: '/3dmodels/sockets/single-switch.glb',
+    position: [-8, 4.6, 3.81],
+    rotation: [Math.PI / 2, 0, -Math.PI / 1],
+  },
+
+  {
+    name: 'BedRoomB-light-switch',
+    model: '/3dmodels/sockets/single-switch.glb',
+    position: [-5.85, 4.6, 3.98],
+    rotation: [Math.PI / 2, 0, -Math.PI / 1],
+  },
+
+  {
+    name: 'Bathroom-light-switch',
+    model: '/3dmodels/sockets/single-switch.glb',
+    position: [-5.55, 4.6, 4.1],
+    rotation: [Math.PI / 2, 0, 0],
+  },
+
+  {
+    name: 'Office-light-switch',
+    model: '/3dmodels/sockets/single-switch.glb',
+    position: [-5.85, 4.6, 6.5],
     rotation: [Math.PI / 2, 0, Math.PI / 2],
   },
 ];
