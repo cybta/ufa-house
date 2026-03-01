9c3bae6108a9c6966e8963e7dd842dbd8196b68b
cybta <c.saad@live.com>
2026-03-01 23:57:44 +0500

    fix radiators utility room and entrance

--- src/pages/HouseScene/data/interactiveModels.ts
+++ src/pages/HouseScene/data/interactiveModels.ts
@@ -4,7 +4,7 @@ export const interactiveModels: ThreeModel[] = [
   {
     name: 'entrance-light-switch',
     model: '/3dmodels/sockets/single-switch.glb',
-    position: [-4.15, 1.6, 5.4],
+    position: [-4.15, 1.6, 4.25],
     rotation: [Math.PI / 2, 0, Math.PI / 2],
   },
 
